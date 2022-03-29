6c235572e6f5cf2eea101035ef096562f73ae326
Corey Stacy <stacy.corey1992@gmail.com>
2022-03-28 23:39:10 -0400

    objects

--- Aspiration/src/main/java/pageObjects/BaseObjects.java
+++ Aspiration/src/main/java/pageObjects/BaseObjects.java
@@ -12,13 +12,13 @@ public class BaseObjects {
 	
 	//Header menu icon and contents
 	By menuIcon = By.xpath("//*[@id=\"__next\"]/div/header/ul[3]/li/svg");
-	By spendNSave = By.xpath("/html/body/div[5]/div/div/div/div[1]/div[2]/a");
-	By credit = By.xpath("/html/body/div[5]/div/div/div/div[1]/div[3]/a");
-	By enterprise = By.xpath("");
-	By whoWeAre = By.xpath("");
-	By impact = By.xpath("");
-	By getStartedBtn = By.xpath("/html/body/div[5]/div/div/div/div[2]/button");
-	By signInBtn = By.xpath("/html/body/div[5]/div/div/div/div[2]/a");
+	By spendNSave = By.xpath("//*[@id=\"__next\"]/div/header/ul[1]/li[1]/a");
+	By credit = By.xpath("//*[@id=\"__next\"]/div/header/ul[1]/li[2]/a");
+	By enterprise = By.xpath("//*[@id=\"__next\"]/div/header/ul[1]/li[3]/a");
+	By whoWeAre = By.xpath("//*[@id=\"__next\"]/div/header/ul[1]/li[4]/a");
+	By impact = By.xpath("//*[@id=\"__next\"]/div/header/ul[1]/li[5]/a");
+	By getStartedBtn = By.xpath("//*[@id=\"__next\"]/div/header/ul[2]/li[2]/button");
+	By signInBtn = By.xpath("//*[@id=\"__next\"]/div/header/ul[2]/li[1]/a");
 	
 	public WebElement clicklogo() {
 		return driver.findElement(logo);
