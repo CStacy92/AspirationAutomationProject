f8c2ef7a092501cb70755bdc827698576bcf5be8
Corey Stacy <stacy.corey1992@gmail.com>
2022-03-28 23:30:46 -0400

    pg objects

---
+++ Aspiration/src/main/java/pageObjects/SpendNSaveObjects.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+
+public class SpendNSaveObjects extends BaseObjects{
+
+	By slogan = By.xpath("//*[@id=\"__next\"]/div/section[1]/div/div[1]/h1");
+	By spendSustHeading = By.xpath("//*[@id=\"__next\"]/div/section[2]/div/h2");
+	By saveMoneyHeading = By.xpath("//*[@id=\"__next\"]/div/section[3]/div[2]/div[1]/h2");
+	By plansHeading = By.xpath("//*[@id=\"__next\"]/div/section[7]/div/div[1]/div[1]/h2");
+	
+}
